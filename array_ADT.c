8a5f2fe28b67f1c8ff65e71bbf54597946d956e0
Satriyo <pamungkasjati9@gmail.com>
2023-01-03 10:23:03 +0700

    Add Binary Search, Rec BinSearch ... Get, Set,min,max, average, sum , ... start reverse array

--- array_ADT.c
+++ array_ADT.c
@@ -47,7 +47,7 @@ void Display(struct Array arr)
 
     printf("\nElements data:\n");
     for (i = 0; i < arr.len ; i++){
-        printf("%d", arr.A[i]);
+        printf("%d ", arr.A[i]);
     }
     printf("\n");
 }
@@ -144,6 +144,134 @@ void swap(int *x, int *y)
     *y = temp;
 }
 
+int BinSearch(struct Array arr, int key)
+{
+    int l,mid,h;
+    l=0;
+    h=arr.len-1;
+
+    while (l <= h)
+    {
+        //calculate mid
+        mid = (l+h)/2;
+
+        //if equal then found, return index
+        if (key == arr.A[mid])
+            return mid;
+        //if key below mid point, update h
+        else if (key < arr.A[mid])
+        {
+            h = mid - 1; //update h index 
+        }
+        //if key above mid point, update low
+        else if (key > arr.A[mid])
+        {
+            l= mid + 1; //update low index
+        }
+    }
+
+    return -1;//not found key in array
+}
+
+int RBinSearch(struct Array arr, int l, int h , int key)
+{
+    if (l <= h)
+    { 
+        int mid = (l+h)/2;
+        if (key==arr.A[mid])
+        {
+            return mid;
+        }
+        else if(key < arr.A[mid])
+        {
+            return RBinSearch(arr, l, mid-1,key);
+        }
+        else if(key > arr.A[mid])
+        {
+            return RBinSearch(arr, mid+1, h, key);
+        }
+    }
+}
+
+
+//Operation in array ADT
+/*
+1. Get (x)
+
+    if ( index >= 0 && index < length)
+        return A[index] O(1)
+2. Set (x, index)
+    if ( index >= 0 && index < length)
+        return A[index] = x O(1)
+3. Max (x)
+    unsorted :
+
+4. Min (x)
+5. Sum (x)
+6. Avg (x)
+*/
+
+int max(struct Array arr)
+{   
+    /*
+    Time complexity O(n)
+
+    */
+    int max = arr.A[0];
+    for (int i=1;i<arr.len;i++)
+    {
+        if (arr.A[i] > max)
+        {
+            max = arr.A[i];
+        }
+    }
+    return max;
+}
+
+int min (struct Array arr)
+{
+    int min = arr.A[0];
+    for(int i = 1;i<arr.len;i++)
+    {
+        if (arr.A[i] < min)
+        min = arr.A[i];
+    }
+    return min;
+}
+
+int sum (struct Array arr)
+{
+    int Total = 0;
+    for(int i=0 ; i < arr.len ; i++)
+    {
+        Total += arr.A[i];
+    }
+    return Total;
+}
+
+int avg (struct Array arr)
+{
+    int Total = 0;
+    Total  = sum(arr);
+    return Total/arr.len;
+}
+
+//Reversing Elements of array :
+/*
+    1. Reverse 
+    2. Left Shift
+    3. Left Rotate
+    4. Right Shift
+    5. Right rotate
+*/
+
+void Reverse(struct Array arr)
+{
+    for (int i = arr.len , int j = 0;i>=0; i--,j++)
+    {
+        B[j]
+    }
+}
 int main()
 {
     struct Array arr;
@@ -166,8 +294,12 @@ int main()
     arr.len = n ;
 
     Display(arr);
-    Delete(&arr, 3);
+    // Delete(&arr, 3);
     Display(arr);
-    printf("Searching number of %d\n", 12);
-    printf("Result search in index : %d", LinearSearch(arr, 12));
+    printf("Searching number of %d\n", 15);
+    printf("\nResult search in index : %d", LinearSearch(arr, 15));
+    printf("\nResult using BinSearch in index: %d", BinSearch(arr, 15));
+    printf("\nMax value of array : %d", max(arr));
+    printf("\nMin value of array : %d", min(arr));
+    printf("\nSum of array is : %d", sum(arr));
 }
